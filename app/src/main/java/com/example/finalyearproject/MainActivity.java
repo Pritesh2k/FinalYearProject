ce32f5a11dafa4321c53ee9f25ffd4d43fd6ba45
Pritesh2k <92019936+Pritesh2k@users.noreply.github.com>
2023-01-06 16:25:12 +0000

    Added Both Read and Write to Firebase DB with Writing Commented out for now. Next steps is to add in all the data from the new Nov 2022 police data set to Firebase

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.finalyearproject;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,27 +31,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Map<String, Object> RecentCrime = new HashMap<>();
-        RecentCrime.put("CrimeID", "dc6a77d7098065e15cf1f3ca960a302058ca11042775b0728f40da2d8dd89a8d");
-        RecentCrime.put("Longitude", "-0.09154");
-        RecentCrime.put("Latitude", "51.511896");
-        RecentCrime.put("Location", "On or near Cannon Street");
-        RecentCrime.put("Crime type", "Drugs");
-        RecentCrime.put("Last outcome category", "Awaiting court outcome");
-
-
-        db.collection("Crime")
-                .add(RecentCrime)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("December2022")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Toast.makeText(getApplicationContext(), document.getString("Crime type"), Toast.LENGTH_LONG).show();
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "UnSuccessful", Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
+
+//        Map<String, Object> RecentCrime = new HashMap<>();
+//        RecentCrime.put("Longitude", "-0.09154");
+//        RecentCrime.put("Latitude", "51.511896");
+//        RecentCrime.put("Location", "On or near Cannon Street");
+//        RecentCrime.put("Crime type", "Drugs");
+//        RecentCrime.put("Last outcome category", "Awaiting court outcome");
+//
+//
+//        db.collection("December2022")
+//                .add(RecentCrime)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+//                    }
+//                });
     }
 }
