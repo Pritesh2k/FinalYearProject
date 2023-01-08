519d2e837f05d5b4705f4b71b7ce7fbc46dd0b5d
Pritesh2k <92019936+Pritesh2k@users.noreply.github.com>
2023-01-08 23:36:33 +0000

    Added a working template for 'Nearest Crime' pulling relevant data from the DB. Implemented a WORKING Saftey Button that Dials (not calls) a number. implemented a working template for an alert message system including a dismiss button. To consider: * Cache issue - causing crashes * Updating the DB (potentially manually) * Fixing the Map * Finishing the Alerting system (as much as possible until there is a haulting factor) * Redesign where you see fit

--- app/src/main/java/com/example/finalyearproject/MainActivity.java
+++ app/src/main/java/com/example/finalyearproject/MainActivity.java
@@ -1,38 +1,88 @@
 package com.example.finalyearproject;
 
-import static android.content.ContentValues.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.cache.Cache;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Referencing Database Instance
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    //Referencing Button Components
     Button GO_TO_MAP_BTN;
+    Button POP_UP_BTN;
+    Button SAFTEY_BTN;
+
+    //Referencing Text Components
+    TextView CRIME_TYPE;
+    TextView LOCATION;
+    TextView LONGITUDE;
+    TextView LATITUDE;
+    TextView SOURCE;
+
+    TextView ALERT_MESSAGE;
+
+    //Variable
+    boolean RESTRICT_UPDATING_DB = false;
+    boolean isAlertMessage_ACTIVE = true;
+    boolean ALERT_MSG_ACTIVE = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Considering the state of the Alert Message Pop Up and setting up the Vibration
+        ALERT_MESSAGE = (TextView) findViewById(R.id.ALERT_MESSAGE);
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        if(isAlertMessage_ACTIVE == true) { //Alert Message is active
+            // Set the new state
+            ALERT_MESSAGE.setVisibility(View.VISIBLE);
+
+            //Vibration code
+            if (vibrator.hasVibrator()) {
+                vibrator.vibrate(1500); // 500 milliseconds
+                try {
+                    Thread.sleep(1000); // 1000 milliseconds = 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                vibrator.vibrate(1000); // 500 milliseconds
+
+            }
+        }
+
+        //Getting the TextView_Data component via ID - To update with the nearest crime
+        CRIME_TYPE = (TextView) findViewById(R.id.Crime_Type_Data);
+        LOCATION = (TextView) findViewById(R.id.Location_Data);
+        LONGITUDE = (TextView) findViewById(R.id.Longitude_Data);
+        LATITUDE = (TextView) findViewById(R.id.Latitude_Data);
+        SOURCE = (TextView) findViewById(R.id.Source_Data);
 
         //On click listener allowing user to click the button to show map
         GO_TO_MAP_BTN = (Button) findViewById(R.id.button);
@@ -45,6 +95,60 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Creating a manual Pop Up Message (MUST incorporate later FULLY along with predication ect...
+        POP_UP_BTN = (Button) findViewById(R.id.pop_up);
+        if (ALERT_MSG_ACTIVE == true){
+            POP_UP_BTN.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    isAlertMessage_ACTIVE = false;
+                    ALERT_MESSAGE.setVisibility(View.INVISIBLE);
+                    POP_UP_BTN.setVisibility(View.GONE);
+                }
+            });
+        }
+
+        //Call someone upon OnClick
+        SAFTEY_BTN = (Button) findViewById(R.id.saftey_button);
+        SAFTEY_BTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Creating new intent and establishing the data
+                try {
+                    //Making the call
+                    Intent safteyButton_Intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678901"));
+                    startActivity(safteyButton_Intent);
+                } catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Call UnSuccessful", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        if (RESTRICT_UPDATING_DB == true) {
+            //Writing to the Firebase DB
+            Map<String, Object> RecentCrime = new HashMap<>();
+            RecentCrime.put("Longitude", "-0.09154");
+            RecentCrime.put("Latitude", "51.511896");
+            RecentCrime.put("Location", "On or near Cannon Street");
+            RecentCrime.put("Crime type", "Drugs");
+            RecentCrime.put("Last outcome category", "Awaiting court outcome");
+
+
+            db.collection("December2022")
+                    .add(RecentCrime)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+
         //Reading from the Firebase DB
         db.collection("December2022")
                 .get()
@@ -53,36 +157,16 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Toast.makeText(getApplicationContext(), document.getString("Crime type"), Toast.LENGTH_LONG).show();
-                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                CRIME_TYPE.setText(document.getString("Crime type"));
+                                LOCATION.setText(document.getString("Location"));
+                                LONGITUDE.setText(document.getString("Longitude"));
+                                LATITUDE.setText(document.getString("Latitude"));
+                                SOURCE.setText("Police.Data.UK");
                             }
                         } else {
                             Toast.makeText(getApplicationContext(), "UnSuccessful", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
-
-        //Writing to the Firebase DB
-//        Map<String, Object> RecentCrime = new HashMap<>();
-//        RecentCrime.put("Longitude", "-0.09154");
-//        RecentCrime.put("Latitude", "51.511896");
-//        RecentCrime.put("Location", "On or near Cannon Street");
-//        RecentCrime.put("Crime type", "Drugs");
-//        RecentCrime.put("Last outcome category", "Awaiting court outcome");
-//
-//
-//        db.collection("December2022")
-//                .add(RecentCrime)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
-//                    }
-//                });
     }
 }
