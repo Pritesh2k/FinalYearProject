e69df33689197fb0ba349e70007e9159e615296a
Pritesh2k <92019936+Pritesh2k@users.noreply.github.com>
2023-02-26 16:44:50 +0000

    successfully implemented News API into APP

---
+++ app/src/main/java/com/example/finalyearproject/Model/NewsAdapter.java
@@ -0,0 +1,87 @@
+package com.example.finalyearproject.Model;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.finalyearproject.R;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsHolder> {
+    private final LayoutInflater layoutInflater;
+    private final Activity activity;
+    private final ArrayList<NewsModel> newsModelArrayList;
+
+
+    public NewsAdapter( Activity activity, ArrayList<NewsModel> newsModelArrayList) {
+        layoutInflater = LayoutInflater.from(activity);
+        this.activity = activity;
+        this.newsModelArrayList = newsModelArrayList;
+    }
+
+    @NonNull
+    @Override
+    public NewsHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=layoutInflater.inflate(R.layout.layout_recyclerview,parent,false);
+        NewsHolder newsHolder=new NewsHolder(view,viewType);
+        return newsHolder;
+    }
+    /*For same return item*/
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull NewsHolder holder, int position) {
+
+        tvTitle.setText(newsModelArrayList.get(position).getTitle());
+        tvPublishAt.setText(newsModelArrayList.get(position).getPublisDate());
+        Glide.with(activity).load(newsModelArrayList.get(position).getUrlToImage()).into(img);
+        Log.i("MARDUK URL",newsModelArrayList.get(position).getUrl());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsModelArrayList.size();
+    }
+    TextView tvTitle,tvContent,tvPublishAt;
+    ImageView img;
+    public class NewsHolder extends RecyclerView.ViewHolder {
+        public NewsHolder(@NonNull View itemView,int viewtype) {
+            super(itemView);
+            tvTitle=itemView.findViewById(R.id.tvTitle);
+            //tvContent=itemView.findViewById(R.id.tvContent);
+            tvPublishAt=itemView.findViewById(R.id.tvPublishAt);
+            img=itemView.findViewById(R.id.img);
+            img.setVisibility(View.GONE);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent=new Intent(activity,DetailsActivity.class);
+                    intent.putExtra("URL",newsModelArrayList.get(viewtype).getUrl());
+                    activity.startActivity(intent);
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/com/example/finalyearproject/NewsAPI.java
+++ app/src/main/java/com/example/finalyearproject/NewsAPI.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.finalyearproject.Model.NewsAdapter;
+import com.example.finalyearproject.Model.NewsModel;
 import com.kwabenaberko.newsapilib.NewsApiClient;
 import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
@@ -20,13 +22,58 @@ import java.util.ArrayList;
 
 public class NewsAPI extends AppCompatActivity {
 
-    Button newsSafteyButton;
+    static Button newsSafteyButton;
+
+    NewsApiClient newsApiClient;
+    ArrayList<NewsModel> newsModelArrayList;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_api);
 
+        recyclerView=findViewById(R.id.recyclerView);
+        newsModelArrayList=new ArrayList<>();//create custom arraylist
+        newsApiClient= new NewsApiClient("8ec91d84acb44334963439c4c7b027c3");
+
+        newsApiClient.getEverything(
+                new EverythingRequest.Builder()
+                        .q("crime+in+london")
+                        .language("en")
+                        .sortBy("publishedAt")
+                        .build(),
+                new NewsApiClient.ArticlesResponseCallback() {
+                    @Override
+                    public void onSuccess(ArticleResponse response) {
+                        Log.i("MARDUK",response.getArticles().get(0).getTitle());
+
+                        //add 70 article to newsmoderarraylist
+                        for (int i = 0; i < 70 ; i++) {
+                            newsModelArrayList.add(new NewsModel(response.getArticles().get(i).getTitle(),response.getArticles().get(i).getAuthor(),response.getArticles().get(i).getContent(),response.getArticles().get(i).getDescription(),response.getArticles().get(i).getUrl(),response.getArticles().get(i).getPublishedAt(),response.getArticles().get(i).getUrlToImage(),response.getArticles().get(i).getSource().getName()));
+                        }
+
+                        //load data to recylerview
+                        NewsAdapter newsAdapter=new NewsAdapter(NewsAPI.this,newsModelArrayList);
+                        recyclerView.setAdapter(newsAdapter);
+                        LinearLayoutManager linearLayoutManager =
+                                new LinearLayoutManager(NewsAPI.this);
+                        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                        recyclerView.setLayoutManager(linearLayoutManager);
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        System.out.println(throwable.getMessage());
+                    }
+                }
+        );
+
+        SafetyButtonHandler();
+    }
+
+    private void SafetyButtonHandler(){
         newsSafteyButton = findViewById(R.id.news_saftey_button);
         newsSafteyButton.setOnClickListener(new View.OnClickListener() {
             @Override
